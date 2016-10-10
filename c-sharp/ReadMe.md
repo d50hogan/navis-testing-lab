1481f12e31925759091ac45ad5d90147ce363d40
Jeff Slavin <jdslavin@thenavisway.com>
2016-10-10 15:54:36 -0700

    updated c sharp files

--- c-sharp/ReadMe.md
+++
@@ -1,1 +0,0 @@
-This project is for the the Concentrated trainight on testing technique talk. 
--- c-sharp/SampleApp/IBaseValue.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SampleApp
-{
-    public interface IRepository
-    {
-        int GetBaseValue();
-    }
-}
